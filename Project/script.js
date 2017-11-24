fe9de5cf39603c5f51d6f53015c30facf94a1b85
aquelemiguel <miguelmanofernandes@gmail.com>
2017-11-24 12:59:41 +0000

    este commit nao tem sexo

--- Project/script.js
+++ Project/script.js
@@ -1,4 +1,3 @@
-
 let todo_add_button = document.getElementById('todo_add_button');
 let todo_specs = document.getElementById('list_adder');
 
@@ -8,35 +7,50 @@ let todo_specs = document.getElementById('list_adder');
 **/
 todo_add_button.addEventListener('click', (event) => {
 
+	// Alerts the user todo list will be deleted.
 	if (todo_specs.children.length)
 		console.log('Deleting unsaved content!');
 
 	// Reset the to-do list by deleting the title and every bullet.
-	while (todo_specs.firstChild) {
+	while (todo_specs.firstChild)
 		todo_specs.removeChild(todo_specs.firstChild);
-	}
 
+	// Creates a new title element.
 	let todo_title = document.createElement('input');
 	todo_title.setAttribute('type', 'text');
 	todo_title.setAttribute('placeholder', 'Title');
-	todo_title.addEventListener('keypress', function(event) {bullet_factory(event)});
+
+	// When the ENTER key is pressed, a new bulletpoint's created.
+	todo_title.addEventListener('keydown', (event) => {
+		if (event.keyCode == 13)
+			bullet_factory();
+	});
 
 	todo_specs.append(todo_title);
 	todo_title.focus();
 });
 
-function bullet_factory(event) {
+/* Creates a new bullet point element. */
+function bullet_factory() {	
+	let bullet = document.createElement('input');
+	bullet.setAttribute('type', 'text');
+	bullet.setAttribute('placeholder', 'Item');
 
-	// Triggered when the ENTER key is pressed.
-	if (event.keyCode == 13) {
+	bullet.addEventListener('keydown', (event) => {
 
-		// Creates a new bullet point.
-		let todo_bullet = document.createElement('input');
-		todo_bullet.setAttribute('type', 'text');
-		todo_bullet.setAttribute('placeholder', 'Item');
+		// A new bulletpoint is created when ENTER's clicked on it.
+		if (event.keyCode == 13)
+			bullet_factory(); // This recursive call surprisingly works.
 
-		todo_specs.append(todo_bullet);
-		todo_bullet.focus();
-		console.log('New bullet created!');
-	} 
+		// The current bulletpoint's deleted when end of input is reached.
+		else if (event.keyCode == 8 && bullet.value.length == 0) {
+			bullet.remove();
+			let prev_bullet = document.querySelector('#list_adder > input:last-child');
+			prev_bullet.focus();
+		}
+
+	});
+
+	todo_specs.append(bullet);
+	bullet.focus();
 } 
