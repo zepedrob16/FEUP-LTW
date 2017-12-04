325d3fc65109b9681726c7d0b553e0416c518e44
zepedrob16 <ze_pedro_borges@hotmail.com>
2017-12-04 16:41:00 +0000

    PooP MalaM

--- Project/profile.php
+++ Project/profile.php
@@ -8,6 +8,12 @@
         <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="css/style.css" rel="stylesheet">
+
+        <?php
+        include_once('includes/init.php');
+        include_once('databases/user.php');
+        ?>
+
     </head>
     <body>
         <header id="mainHeader">
@@ -35,9 +41,35 @@
         <div class="content">
             <div id="info">
                 <img id="profilePic" style="vertical-align:middle" src="resources/bust_a_telmo.jpg" height="200">
-                <span>Name: Penios Erectus</span>
-                <span>Username: bustanut</span>
-                <span>E-mail: up201504614@fe.up.pt</span>
+                <span>
+                    <?php 
+                    global $dbh;
+                    $stmt = $dbh->prepare("SELECT name FROM users");
+                    $stmt->execute();   
+                    $array_info = $stmt->fetch();
+                    $string_version = implode(',', $array_info);
+                    echo $string_version;
+                ?>
+                </span>
+                <span>
+                    <?php 
+                    global $dbh;
+                    $stmt = $dbh->prepare("SELECT username FROM users");
+                    $stmt->execute();   
+                    $array_info = $stmt->fetch();
+                    $string_version = implode(',', $array_info);
+                    echo "Username: " . $string_version;
+                ?>
+                </span>
+                <span>
+                    <?php 
+                    global $dbh;
+                    $stmt = $dbh->prepare("SELECT email FROM users");
+                    $stmt->execute();   
+                    $array_info = $stmt->fetch();
+                    $string_version = implode(',', $array_info);
+                    echo "Email: " . $string_version;
+                ?></span>
             </div>
         </div>
         <footer>
