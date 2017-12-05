a72017e1bae99f5d36d670bc0884a21fc97062c0
zepedrob16 <ze_pedro_borges@hotmail.com>
2017-12-05 19:46:22 +0000

    ups

--- Project/signup.php
+++ Project/signup.php
@@ -7,7 +7,7 @@ if (new_account($_POST['username'], $_POST['password'], $_POST['firstName'], $_P
 	$message = "Account created";
 	echo "<script type='text/javascript'>alert('$message');</script>";
 
-	header('Location: mainPage.html');
+	header('Location: mainPage.php');
 	exit;
 }
 ?>
