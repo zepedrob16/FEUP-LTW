e2cacd56b4fff0c2cfe688dcec2ce2d04a8d3506
zepedrob16 <ze_pedro_borges@hotmail.com>
2017-12-13 12:26:59 +0000

    Progress in obtaining all information

--- Project/list-functions.php
+++ Project/list-functions.php
@@ -5,45 +5,46 @@
     global $dbh;
     $stmt = $dbh->prepare("SELECT id_list FROM List WHERE username = ?");
     $stmt->execute(array($username));   
-    $array_info = $stmt->fetch();
-    $string_version = implode(',', $array_info);
-    echo "ID: " . $string_version;
-  }
+    while ($row = $stmt->fetch()) {
+     echo ' ID ' . $row['id_list'];
+    }
+
+}
 
  function get_title($username) {
     global $dbh;
     $stmt = $dbh->prepare("SELECT title FROM List WHERE username = ?");
     $stmt->execute(array($username));   
-    $array_info = $stmt->fetch();
-    $string_version = implode(',', $array_info);
-    echo "Title: " . $string_version;
+    while ($row = $stmt->fetch()) {
+     echo ' Title ' . $row['title'];
+    }
   }
 
   function get_creation_date($username) {
     global $dbh;
     $stmt = $dbh->prepare("SELECT creation_date FROM List WHERE username = ?");
     $stmt->execute(array($username));   
-    $array_info = $stmt->fetch();
-    $string_version = implode(',', $array_info);
-    echo "Creation Date: " . $string_version;
+    while ($row = $stmt->fetch()) {
+     echo ' Creation Date ' . $row['creation_date'];
+    }
   }
 
   function get_priority($username) {
     global $dbh;
     $stmt = $dbh->prepare("SELECT priority FROM List WHERE username = ?");
     $stmt->execute(array($username));   
-    $array_info = $stmt->fetch();
-    $string_version = implode(',', $array_info);
-    echo "Priority: " . $string_version;
+    while ($row = $stmt->fetch()) {
+     echo ' Priority ' . $row['priority'];
+    }
   }
 
   function get_tags($username) {
     global $dbh;
     $stmt = $dbh->prepare("SELECT tags FROM List WHERE username = ?");
     $stmt->execute(array($username));   
-    $array_info = $stmt->fetch();
-    $string_version = implode(',', $array_info);
-    echo "Tags: " . $string_version;
+    while ($row = $stmt->fetch()) {
+     echo ' Tags ' . $row['tags'];
+    }
   }
 
 
