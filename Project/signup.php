e77625d075bc19c5a4a7ab215270654dddb4ef3c
aquelemiguel <miguelmanofernandes@gmail.com>
2017-12-15 00:39:52 +0000

    peidolas

--- Project/signup.php
+++ Project/signup.php
@@ -4,17 +4,17 @@ include_once('includes/init.php');
 include_once('databases/user.php');
 
 if(!preg_match("/^[a-zA-Z0-9\s]+$/", $_POST['username'])) {
-	header('Location: ' . urlencode('register.html'));
+	header('Location: ' . urlencode('register.php'));
 	exit;
 }
 
 else if (new_account($_POST['username'], $_POST['password'], $_POST['firstName'], $_POST['lastName'], $_POST['email'])) {
 
-	header('Location: ' . urlencode('index.html'));
+	header('Location: ' . urlencode('sign_in.php'));
 	exit;
 }
 
 else {
-	header('Location: ' . urlencode('register.html'));
+	header('Location: ' . urlencode('register.php'));
 }
 ?>
