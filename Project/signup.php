e96f1a391e6fb8bf0651613aa18b084f1f1c3308
zepedrob16 <ze_pedro_borges@hotmail.com>
2017-12-15 09:58:46 +0000

    MAIS SEGURANÇA

--- Project/signup.php
+++ Project/signup.php
@@ -8,6 +8,16 @@ if(!preg_match("/^[a-zA-Z0-9\s]+$/", $_POST['username'])) {
 	exit;
 }
 
+if(!preg_match("/^[a-zA-Z0-9\s]+$/", $_POST['firstName'])) {
+	header('Location: ' . urlencode('register.php'));
+	exit;
+}
+
+if(!preg_match("/^[a-zA-Z0-9\s]+$/", $_POST['lastName'])) {
+	header('Location: ' . urlencode('register.php'));
+	exit;
+}
+
 else if (new_account($_POST['username'], $_POST['password'], $_POST['firstName'], $_POST['lastName'], $_POST['email'])) {
 
 	header('Location: ' . urlencode('sign_in.php'));
